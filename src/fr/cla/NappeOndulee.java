7a030af40cc81303e93d2b8973f97e3b24d32686
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-15 15:24:56 +0100

    NappeOndulee OK

--- src/fr/cla/NappeOndulee.java
+++ src/fr/cla/NappeOndulee.java
@@ -2,28 +2,28 @@ package fr.cla;
 
 import java.io.IOException;
 import static java.lang.Math.sin;
-import static java.lang.Math.sqrt;
 
-public class Nappe extends AbstractDessinRgb {
+public class NappeOndulee extends AbstractDessinRgb {
 
     public static void main(String[] args) throws IOException {
-        new Nappe().dessine();
+        new NappeOndulee().dessine();
     }
 
     @Override protected int size() { return 1024; }
 
     @Override protected int r(int i, int j, int size) {
-        double s=3./(j+99);
-        double y=(j+sin((i*i+sqrt(j-700)*5)/100./size)*35)*s;
+        float s=3.0F/(j+99);
+        float y= (float) ((j+sin((i*i+_sq(j - 700)*5)/100.0F/size)*35)*s);
         return (
                 ((int)((i+size)*s+y))%2
                 +
                 ((int)((size*2-i)*s+y))%2
         )*127;
     }
+
     @Override protected int g(int i, int j, int size) {
-        double s=3./(j+99);
-        double y=(j+sin((i*i+sqrt(j-700)*5)/100./size)*35)*s;
+        float s=3.0F/(j+99);
+        float y= (float) ((j+sin((i*i+_sq(j - 700)*5)/100./size)*35)*s);
         return (
                 ((int)(5*((i+size)*s+y)))%2
                 +
@@ -31,8 +31,8 @@ public class Nappe extends AbstractDessinRgb {
         )*127;
     }
     @Override protected int b(int i, int j, int size) {
-        double s=3./(j+99);
-        double y=(j+sin((i*i+sqrt(j-700)*5)/100./size)*35)*s;
+        float s=3.0F/(j+99);
+        float y= (float) ((j+sin((i*i+_sq(j - 700)*5)/100./size)*35)*s);
         return (
                 ((int)(29*((i+size)*s+y)))%2
                 +
@@ -40,4 +40,7 @@ public class Nappe extends AbstractDessinRgb {
         )*127;
     }
 
+    private int _sq(int i) {
+        return i*i;
+    }
 }
