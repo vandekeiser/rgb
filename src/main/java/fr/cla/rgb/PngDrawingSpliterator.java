a5fa86dc29ee9729320a3be16679f02ca1e2ce77
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-16 23:00:09 +0100

    neutralSplit ok

--- src/main/java/fr/cla/rgb/PngDrawingSpliterator.java
+++ src/main/java/fr/cla/rgb/PngDrawingSpliterator.java
@@ -5,15 +5,30 @@ import java.util.function.Consumer;
 
 public class PngDrawingSpliterator implements Spliterator<PngDrawing> {
 
+    private static final int MAX_DRAWING_SIZE = 1024;
     private PngDrawing drawing;
+    private int tilesLeft;
 
     public PngDrawingSpliterator(PngDrawing drawing) {
         this.drawing = drawing;
+        this.tilesLeft = nbOfTiles(drawing);
+    }
+
+    private int nbOfTiles(PngDrawing drawing) {
+        int drawingSize = drawing.size();
+        int tilesQuotient = MAX_DRAWING_SIZE % drawingSize;
+        int tilesRemainder = MAX_DRAWING_SIZE % drawingSize;
+
+        if(tilesRemainder !=0 ) {
+            throw new UnsupportedOperationException("fffff: "+drawingSize + " , " + MAX_DRAWING_SIZE);
+        }
+
+        return tilesQuotient;
     }
 
     @Override public boolean tryAdvance(Consumer<? super PngDrawing> action) {
-        //action.accept();
-        return false;
+        action.accept(drawing);
+        return --tilesLeft==0;
     }
 
     @Override public Spliterator<PngDrawing> trySplit() {
--- src/test/java/fr/cla/rgb/PngDrawingSplitTest.java
+++ src/test/java/fr/cla/rgb/PngDrawingSplitTest.java
@@ -1,6 +1,8 @@
 package fr.cla.rgb;
 
+import java.util.Set;
 import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.assertEquals;
 
 import fr.cla.rgb.examples.FirstTry;
@@ -8,9 +10,10 @@ import org.junit.Test;
 
 public class PngDrawingSplitTest {
 
-	
 	@Test public void neutralSplit() {
         PngDrawing smallDrawing = new FirstTry(128);
         Stream<PngDrawing> smallDrawingSplit = smallDrawing.split();
+        Set<PngDrawing> tiles = smallDrawingSplit.collect(toSet());
+        assertEquals(1, tiles.size());
 	}
 }
