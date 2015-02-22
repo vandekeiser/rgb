bf36af03487ee375803b556ff8d0905d55c6a3e5
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-22 16:29:11 +0100

    AsyncTilingDrawer

--- src/main/java/fr/cla/rgb/drawer/AsyncTilingDrawer.java
+++ src/main/java/fr/cla/rgb/drawer/AsyncTilingDrawer.java
@@ -5,16 +5,14 @@ import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 import javax.imageio.ImageIO;
 import fr.cla.rgb.drawing.Drawing;
-import fr.cla.rgb.drawing.Tile;
 import fr.cla.rgb.drawing.WholeDrawing;
 
-public class ParallelConcurrentTilingDrawer extends ParallelTilingDrawer {
+public class AsyncTilingDrawer extends ParallelTilingDrawer {
 
-    public static final ParallelConcurrentTilingDrawer INSTANCE = new ParallelConcurrentTilingDrawer();
-    private ParallelConcurrentTilingDrawer() {}
+    public static final AsyncTilingDrawer INSTANCE = new AsyncTilingDrawer();
+    private AsyncTilingDrawer() {}
     
     @Override protected void writeTiles(WholeDrawing drawing, Path tempTilesPath) {
         int nbOfLines = drawing.nbOfLines();
--- src/test/java/fr/cla/rgb/drawer/ParallelConcurrentTilingDrawerTest.java
+++ src/test/java/fr/cla/rgb/drawer/ParallelConcurrentTilingDrawerTest.java
@@ -3,7 +3,7 @@ package fr.cla.rgb.drawer;
 public class ParallelConcurrentTilingDrawerTest extends TilingDrawerTest {
 
     @Override protected Drawer createDrawer() {
-        return ParallelConcurrentTilingDrawer.INSTANCE;
+        return AsyncTilingDrawer.INSTANCE;
     }
     
 }
