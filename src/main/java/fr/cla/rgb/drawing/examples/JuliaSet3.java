2b4371c0f780d01d0041a0687fd22cb4ae6d71b4
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-28 21:05:00 +0100

    JuliaSet3_dragon_16384_MAXIT512_TAU20.png

--- src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
+++ src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
@@ -4,6 +4,7 @@ import java.util.IntSummaryStatistics;
 import fr.cla.rgb.drawer.ParallelAsyncTilingDrawer;
 import fr.cla.rgb.drawing.Point;
 import fr.cla.rgb.drawing.WholeDrawing;
+import static java.lang.Math.*;
 
 public class JuliaSet3 extends WholeDrawing {
 
@@ -12,16 +13,26 @@ public class JuliaSet3 extends WholeDrawing {
     public JuliaSet3(int size) {
         super(size);
     }
-                                              //JuliaSet3_16384_MAXIT32.png
+
     public static void main(String... args) throws Exception {
-        JuliaSet3 js = new JuliaSet3(1024);
+        JuliaSet3 js = new JuliaSet3(16384);
+
         System.out.println("Computing color scale..");
         IntSummaryStatistics divergingIterationStats = js.points().parallel().mapToInt(
                 p -> js.divergingIteration(p.x, p.y, js.wholeDrawingSize())
         ).summaryStatistics();
         js.maxDivergingIteration = divergingIterationStats.getMax();
-        System.out.println("Color scale: stats="+divergingIterationStats);
-        
+        System.out.println("Color scale: stats=" + divergingIterationStats);
+        System.out.printf("Color scale: nonlinearWavelength(0)=%.0f%n", js.nonlinearWavelength(0));
+        System.out.printf("Color scale: nonlinearWavelength(maxDivergingIteration/2)=%.0f%n", 
+                js.nonlinearWavelength((int) (js.maxDivergingIteration/2.0)));
+        System.out.printf("Color scale: nonlinearWavelength(maxDivergingIteration*3/4)=%.0f%n", 
+                js.nonlinearWavelength((int) (js.maxDivergingIteration*3.0/4.0)));
+        System.out.printf("Color scale: nonlinearWavelength(maxDivergingIteration)=%.0f%n", 
+                js.nonlinearWavelength(js.maxDivergingIteration));
+        System.out.printf("Color scale: nonlinearWavelength(maxDivergingIteration+1)=%.0f%n", 
+                js.nonlinearWavelength(js.maxDivergingIteration+1));
+
         ParallelAsyncTilingDrawer.INSTANCE.draw(js);
     }
 
@@ -29,43 +40,62 @@ public class JuliaSet3 extends WholeDrawing {
         int x = p.x, y = p.y, s = wholeDrawingsize;
         
         int n = divergingIteration(x, y, s);
-        double lambda = wavelength(n);
+        //double lambda = linearWavelength(n);
+        double lambda = nonlinearWavelength(n);
         int[] rgb = U.waveLengthToRGB(lambda);
         
         return (rgb[0] << 8 | rgb[1]) << 8 | rgb[2];
     }
 
-    private double wavelength(int n) {
-        if(n > MAX_IT) return Double.NaN;
+    private double linearWavelength(int divergingIteration) {
+        if(divergingIteration > MAX_IT) return Double.NaN;
+        double purple = 380.0, red = 780.0, range = red - purple;
+        return purple + (range*1.0/maxDivergingIteration) * divergingIteration;
+    }
+    
+    private double nonlinearWavelength(int divergingIteration) {
+        if(divergingIteration > MAX_IT) return Double.NaN; //black if no divergence detected
 
-        double red = 380.0, purple = 780.0, range = purple - red;
+        double purple = 380.0, red = 780.0, range = red - purple;
         //double red = 540.0, purple = 600.0, range = purple - red;
         //double red = 490.0, purple = 510.0, range = purple - red;
         //double red = 470.0, purple = 500.0, range = purple - red;
+
+        double x = divergingIteration*1.0/maxDivergingIteration; // E [0, 1]
         
-        return red + (range/ maxDivergingIteration) * n;
+        return purple + range * (1.0 - exp(-TAU*x))/(1.0 - exp(-TAU));
     }
-    
-    //bizarrement structure plus jolie qd petit,
-    // plus gros ecrase les couleurs
-    //mais trop petit (8) pas bon non plus.. 32 OK
-    private static int MAX_IT = 32;
+    private static int MAX_IT = 512;
+    //"time constant" of the exponential used to get more detail toward reds
+    private static double TAU = 20.0;
     
     private static int divergingIteration(int i, int j, int size) {
         double x = D(i, size),
               y = D(j, size),
               X, Y; //values before iteration
 
-        //f(z)=z^2+c, c=xadd + iyadd
         //double xadd = 0.36237, yadd = 0.32;     //interessant
         //double xadd = 0, yadd = 0;              //cercle
         //double xadd = 0.03515, yadd = -0.07467; //queud
-        double xadd = -0.672, yadd = 0.435;       //interessant
+        //double xadd = -0.672, yadd = 0.435;       //interessant
+//        Some of the more famous Julia sets are:
+//            C = (-1,0)
+//            C = (0,-1) -- the Dendrite
+//            C = (1/2, 0)
+//            C = (-0.1, 0.8) -- the Rabbit
+//            C = (.36, .1) -- the Dragon
+        //double xadd = -1.0, yadd = 0.0; //semi-interessant
+        //double xadd = 0.0, yadd = -1.0; //"the Dendrite" interessant
+        //double xadd = 0.5, yadd = 0.0; //semi-interessant
+        //double xadd = -0.1, yadd = 0.8; //"the Rabbit" interessant
+        double xadd = 0.36, yadd = 0.1; //"the Dragon" interessant
 
         int n = 0;
         while(n++<MAX_IT && (x*x+y*y)<4.0) {//certain divergence outside of the circle of radius 2
             X = x;
             Y = y;
+            //f(z)=z^2+c
+            //c=xadd + i*yadd
             //z=x+iy -> z^2=x^2-y^2 + 2i*x*y
             x = X*X - Y*Y + xadd;
             y = 2*X*Y     + yadd;
@@ -82,12 +112,12 @@ public class JuliaSet3 extends WholeDrawing {
 
  
     @Override protected int R(int x, int y, int wholeDrawingsize) {
-            throw new AssertionError();
-        }
-        @Override protected int G(int x, int y, int wholeDrawingsize) {
-            throw new AssertionError();
-        }
-        @Override protected int B(int x, int y, int wholeDrawingsize) {
-            throw new AssertionError();
-        }
+        throw new AssertionError();
+    }
+    @Override protected int G(int x, int y, int wholeDrawingsize) {
+        throw new AssertionError();
+    }
+    @Override protected int B(int x, int y, int wholeDrawingsize) {
+        throw new AssertionError();
+    }
 }
