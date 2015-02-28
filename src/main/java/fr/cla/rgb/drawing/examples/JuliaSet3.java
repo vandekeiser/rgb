b6f8e09d70d14ae9e167d56c1e5f20205b8f7da8
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-28 16:55:01 +0100

    ff

--- src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
+++ src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
@@ -7,7 +7,7 @@ import fr.cla.rgb.drawing.WholeDrawing;
 
 public class JuliaSet3 extends WholeDrawing {
 
-    private int maxn;
+    private int maxDivergingIteration;
 
     public JuliaSet3(int size) {
         super(size);
@@ -16,9 +16,11 @@ public class JuliaSet3 extends WholeDrawing {
     public static void main(String... args) throws Exception {
         JuliaSet3 js = new JuliaSet3(1024);
         System.out.println("Computing color scale..");
-        IntSummaryStatistics stats = js.points().parallel().mapToInt(p -> js.divergingIteration(p.x, p.y, js.wholeDrawingSize())).summaryStatistics();
-        js.maxn = stats.getMax();
-        System.out.println("Color scale: stats="+stats);
+        IntSummaryStatistics divergingIterationStats = js.points().parallel().mapToInt(
+                p -> js.divergingIteration(p.x, p.y, js.wholeDrawingSize())
+        ).summaryStatistics();
+        js.maxDivergingIteration = divergingIterationStats.getMax();
+        System.out.println("Color scale: stats="+divergingIterationStats);
         
         ParallelAsyncTilingDrawer.INSTANCE.draw(js);
     }
@@ -37,16 +39,28 @@ public class JuliaSet3 extends WholeDrawing {
         double red = 380.0, purple = 780.0, range = purple - red;
         //double red = 490.0, purple = 510.0, range = purple - red;   //voit qetchos
         //double red = 470.0, purple = 500.0, range = purple - red;
-        return red + (range/maxn) * n;
+        return red + (range/ maxDivergingIteration) * n;
     }
     
     private static int divergingIteration(int i, int j, int size) {
-        float x=D(i, size),y=D(j, size),X,Y; int n=0;
-        while(n++<600&&(x*x+y*y)<4){X=x;Y=y;x=X*X-Y*Y+0.36237F;y=2*X*Y+0.32F;}
+        float x = D(i, size), 
+              y = D(j, size),
+              X,
+              Y; 
+        
+        int n = 0, MAX_IT = 600;
+        
+        while(n++<MAX_IT && (x*x+y*y)<4.0) {//certain divergence outside of the circle of radius 2
+            X = x;
+            Y = y;
+            x = X*X - Y*Y + 0.36237F;
+            y = 2*X*Y     + 0.32F;
+        }
         return n;
     }
+    
     private static float D(int x, int size) {
-        return (x-size/2.0F)/(size/2.0F);
+        return (x - size/2.0F)/(size/2.0F);
     }
 
  
