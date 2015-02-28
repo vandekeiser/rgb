dc9a3dec9e5ec7aec4f9b699ba8038f1029d3a94
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-02-28 17:15:54 +0100

    plus de floats

--- src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
+++ src/main/java/fr/cla/rgb/drawing/examples/JuliaSet3.java
@@ -29,38 +29,42 @@ public class JuliaSet3 extends WholeDrawing {
         int x = p.x, y = p.y, s = wholeDrawingsize;
         
         int n = divergingIteration(x, y, s);
-        double lambda = lambda(n);
+        double lambda = wavelength(n);
         int[] rgb = U.waveLengthToRGB(lambda);
         
         return (rgb[0] << 8 | rgb[1]) << 8 | rgb[2];
     }
 
-    private double lambda(int n) {
-        double red = 380.0, purple = 780.0, range = purple - red;
-        //double red = 490.0, purple = 510.0, range = purple - red;   //voit qetchos
+    private double wavelength(int n) {
+        //double red = 380.0, purple = 780.0, range = purple - red;
+        double red = 540.0, purple = 600.0, range = purple - red;
+        //double red = 490.0, purple = 510.0, range = purple - red;
         //double red = 470.0, purple = 500.0, range = purple - red;
+        
+        //if(n==1) return Double.NaN;
         return red + (range/ maxDivergingIteration) * n;
     }
     
     private static int divergingIteration(int i, int j, int size) {
-        float x = D(i, size), 
+        double x = D(i, size),
               y = D(j, size),
-              X,
-              Y; 
+              X, Y; //values before iteration
+        int n = 0, MAX_IT = 1024;
         
-        int n = 0, MAX_IT = 600;
-        
+        double xadd = 0.36237,
+               yadd = 0.32;
         while(n++<MAX_IT && (x*x+y*y)<4.0) {//certain divergence outside of the circle of radius 2
             X = x;
             Y = y;
-            x = X*X - Y*Y + 0.36237F;
-            y = 2*X*Y     + 0.32F;
+            //z=x+iy -> z^2=x^2-y^2 + 2i*x*y
+            x = X*X - Y*Y + xadd;
+            y = 2*X*Y     + yadd;
         }
         return n;
     }
     
-    private static float D(int x, int size) {
-        return (x - size/2.0F)/(size/2.0F);
+    private static double D(int x, int size) {
+        return (x - size/2.0)/(size/2.0);
     }
 
  
--- src/main/java/fr/cla/rgb/drawing/examples/U.java
+++ src/main/java/fr/cla/rgb/drawing/examples/U.java
@@ -1,10 +1,14 @@
 package fr.cla.rgb.drawing.examples;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.round;
+
 public enum U {
     ;
+    private static final int[] ZERO = {0, 0, 0};
 
     public static double sq (double x) {
-        return Math.pow(x, 2.0);
+        return pow(x, 2.0);
     }
 
     public static int sq (int x) {
@@ -19,60 +23,58 @@ public enum U {
     /** Taken from Earl F. Glynn's web page:
     * <a href="http://www.efg2.com/Lab/ScienceAndEngineering/Spectra.htm">Spectra Lab Report</a>
     * */
-    public static int[] waveLengthToRGB(double Wavelength){
+    public static int[] waveLengthToRGB(double wavelength){
+        if(Double.isNaN(wavelength)) return ZERO;
+        
         double factor;
         double Red,Green,Blue;
 
-        if((Wavelength >= 380) && (Wavelength<440)){
-            Red = -(Wavelength - 440) / (440 - 380);
+        if((wavelength >= 379) && (wavelength<440)){
+            Red = -(wavelength - 440) / (440 - 380);
             Green = 0.0;
             Blue = 1.0;
-        }else if((Wavelength >= 440) && (Wavelength<490)){
+        }else if((wavelength >= 440) && (wavelength<490)){
             Red = 0.0;
-            Green = (Wavelength - 440) / (490 - 440);
+            Green = (wavelength - 440) / (490 - 440);
             Blue = 1.0;
-        }else if((Wavelength >= 490) && (Wavelength<510)){
+        }else if((wavelength >= 490) && (wavelength<510)){
             Red = 0.0;
             Green = 1.0;
-            Blue = -(Wavelength - 510) / (510 - 490);
-        }else if((Wavelength >= 510) && (Wavelength<580)){
-            Red = (Wavelength - 510) / (580 - 510);
+            Blue = -(wavelength - 510) / (510 - 490);
+        }else if((wavelength >= 510) && (wavelength<580)){
+            Red = (wavelength - 510) / (580 - 510);
             Green = 1.0;
             Blue = 0.0;
-        }else if((Wavelength >= 580) && (Wavelength<645)){
+        }else if((wavelength >= 580) && (wavelength<645)){
             Red = 1.0;
-            Green = -(Wavelength - 645) / (645 - 580);
+            Green = -(wavelength - 645) / (645 - 580);
             Blue = 0.0;
-        }else if((Wavelength >= 645) && (Wavelength<781)){
+        }else if((wavelength >= 645) && (wavelength<781)){
             Red = 1.0;
             Green = 0.0;
             Blue = 0.0;
         }else{
-            Red = 0.0;
-            Green = 0.0;
-            Blue = 0.0;
+            throw new AssertionError();
         };
 
         // Let the intensity fall off near the vision limits
 
-        if((Wavelength >= 380) && (Wavelength<420)){
-            factor = 0.3 + 0.7*(Wavelength - 380) / (420 - 380);
-        }else if((Wavelength >= 420) && (Wavelength<701)){
+        if((wavelength >= 380) && (wavelength<420)){
+            factor = 0.3 + 0.7*(wavelength - 380) / (420 - 380);
+        }else if((wavelength >= 420) && (wavelength<701)){
             factor = 1.0;
-        }else if((Wavelength >= 701) && (Wavelength<781)){
-            factor = 0.3 + 0.7*(780 - Wavelength) / (780 - 700);
+        }else if((wavelength >= 701) && (wavelength<781)){
+            factor = 0.3 + 0.7*(780 - wavelength) / (780 - 700);
         }else{
             factor = 0.0;
         };
 
 
         int[] rgb = new int[3];
-
         // Don't want 0^x = 1 for x <> 0
-        rgb[0] = Red==0.0 ? 0 : (int) Math.round(IntensityMax * Math.pow(Red * factor, Gamma));
-        rgb[1] = Green==0.0 ? 0 : (int) Math.round(IntensityMax * Math.pow(Green * factor, Gamma));
-        rgb[2] = Blue==0.0 ? 0 : (int) Math.round(IntensityMax * Math.pow(Blue * factor, Gamma));
-
+        rgb[0] = Red==0.0 ? 0 : (int) round(IntensityMax * pow(Red * factor, Gamma));
+        rgb[1] = Green==0.0 ? 0 : (int) round(IntensityMax * pow(Green * factor, Gamma));
+        rgb[2] = Blue==0.0 ? 0 : (int) round(IntensityMax * pow(Blue * factor, Gamma));
         return rgb;
     }
 }
