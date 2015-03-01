b8f8e7a883f5916745ced057ee77723296bd495f
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-03-01 23:20:08 +0100

    opencv de mvn central, SimpleSample OK

--- src/main/java/fr/cla/rgb/drawer/opencv/SimpleSample.java
+++ src/main/java/fr/cla/rgb/drawer/opencv/SimpleSample.java
@@ -1,5 +1,6 @@
 package fr.cla.rgb.drawer.opencv;
 
+import com.sun.java.swing.plaf.windows.resources.windows;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.CvType;
@@ -8,6 +9,12 @@ import org.opencv.core.Scalar;
 class SimpleSample {
 
   //static{ System.loadLibrary("opencv_java248"); }
+    //[INFO] Copying opencvjar-runtime-2.4.8-natives-windows-x86_64.jar to g:\projets\
+    //blog\rgb\target\lib\opencvjar-runtime-2.4.8-natives-windows-x86_64.jar
+    //PAS MAJ TJRS EN VERSION 2.4.8..
+    
+    //-Djava.library.path=G:\projets\blog\rgb\opencv-full\opencv\build\java\x64
+    //UnsatisfiedLinkError: no opencv_java2410 in java.library.path
   static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
   public static void main(String... args) {
