152edae7a331ac024691cff66fcd358b6d14925d
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2015-03-02 17:34:01 +0100

    OpenCvTilingTest ok

--- src/main/java/fr/cla/rgb/drawer/opencv/OpenCvTiling.java
+++ src/main/java/fr/cla/rgb/drawer/opencv/OpenCvTiling.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
@@ -17,31 +15,38 @@ public class OpenCvTiling {
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void tile(String[] tilesPaths, String outPath) throws IOException {
+        //1. Get info from 1st tile
+        Mat firstTileMat = Highgui.imread(tilesPaths[0]);
+        int tileRows = firstTileMat.rows(), //whole image / ntiles
+            tileCols = firstTileMat.cols(), //same as whole image
+            ntiles = tilesPaths.length;
+        if(tileRows * ntiles != tileCols) throw new AssertionError();
+        
+        //2. Stream of all tiles
         Stream<String> tilesPathsStream = Arrays.stream(tilesPaths);
         Stream<Mat> tilesMatsStream = tilesPathsStream.map(Highgui::imread);
         
-        Mat firstTileMat = Highgui.imread(tilesPaths[0]);
+        //3. Init whole image
+        Mat outMat = new Mat(tileCols, tileCols, firstTileMat.type());
+        
+        //4. Write all tiles to whole image
+        AtomicInteger itile = new AtomicInteger(0);
+        tilesMatsStream.forEach(tileMat -> {
+            Rect roi = new Rect(
+                0,
+                itile.getAndIncrement() * tileRows,
+                tileCols,
+                tileRows
+            );
+            System.out.println("roi: " + roi);
+            System.out.println("outMat: " + outMat);
 
-        Mat outMat = new Mat(
-                firstTileMat.rows()*tilesPaths.length, 
-                firstTileMat.cols()*tilesPaths.length,
-                firstTileMat.type());
+            Mat outView = outMat.submat(roi);
+
+            tileMat.copyTo(outView);
+        });
         
-        
-        
-        tilesMatsStream.forEach(tilesMat -> 
-//                Imgproc.accumulate(tilesMat, outMat)
-                        tilesMat.copyTo(outMat)
-        );
+        //5. Write whole image to disk
         Highgui.imwrite(outPath, outMat);
-//        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-//        System.out.println("OpenCV Mat: " + m);
-//        Mat mr1 = m.row(1);
-//        mr1.setTo(new Scalar(1));
-//        Mat mc5 = m.col(5);
-//        mc5.setTo(new Scalar(5));
-//        System.out.println("OpenCV Mat data:\n" + m.dump());
-        
-        //Files.createFile(Paths.get("OpenCvTilingTest.png"));
     }
 }
